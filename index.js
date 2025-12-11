0a647e7db6bca0c3ae26f10526b5ef4885b65f8e
Kiana Mills <kiana.mills20@gmail.com>
2025-12-10 16:47:46 -0800

    javascript1

---
+++ index.js
@@ -0,0 +1,15 @@
+let code1 = 10;
+code1 = 7 + 3;
+console.log(code1)
+let code2 = 40;
+code2 = (100 / 2) - 10; 
+console.log(code2)
+let code3 = 39; 
+code3 = 145 % 53;
+console.log(code3)
+const Message = "The vault has been secured. The combination is:";
+console.log(Message)
+const codeA = code1 + "-" + code2 + "-" + code3;
+console.log(codeA)
+const codeB = `${code1}-${code2}-${code3}`;
+console.log(codeB)
